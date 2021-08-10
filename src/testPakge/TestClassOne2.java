5b48121bf9c88fccbcde6cd219d91bbea29c9536
vibhu93 <vaibhav.gawande0@gmail.com>
2021-08-10 14:31:40 +0530

    Vaibhav Chnages Done

--- src/testPakge/TestClassOne2.java
+++ src/testPakge/TestClassOne2.java
@@ -3,7 +3,7 @@ package testPakge;
 public class TestClassOne2 {
 
 	public static void main(String[] args) {
-		System.out.println(" Vaibhav TODO Auto-generated method stub");
+		System.out.println("TODO Auto-generated method stub");
 
 	}
 
