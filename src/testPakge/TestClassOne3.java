0c64dd09786efbbeeb071347ec9d474a4006605a
nikhilg <nikhilg@LXI296.LXI.lumedx.com>
2021-08-10 15:49:36 +0530

    Done By save Button By officePC

--- src/testPakge/TestClassOne3.java
+++ src/testPakge/TestClassOne3.java
@@ -5,7 +5,6 @@ public class TestClassOne3 {
 	public static void main(String[] args) {
 		System.out.println("TODO Auto-generated method stub");
 		System.out.println("Suggestion by the great are done.");
-
 	}
 
 }
